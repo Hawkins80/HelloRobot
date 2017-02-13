e3c99cb828f4e4017a28a0e0190c8954f538ad61
Hawkins80 <hawkinsn@gilmour.org>
2017-02-13 11:54:53 -0500

    dance and twirl longer

--- hello.py
+++ hello.py
@@ -4,13 +4,15 @@ import time
 def shimmy():
     for x in range(2):
         right()
-        time.sleep(.5)
+        time.sleep(1)
         left()
-        time.sleep(.5)
+        time.sleep(1)
 
 def twirl():
     right_rot()
-    time.sleep(2)
+    time.sleep(3)
+    left_rot()
+    time.sleep(3)
 
 shimmy()
 twirl()
